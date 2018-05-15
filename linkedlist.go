f3371dc0f73d6c46c0758ba4e7da1e495c637619
TheRealLucky <i15091@student.htlwrn.ac.at>
2018-05-15 09:59:25 +0200

    Implement Add() and Get() method

--- linkedlist.go
+++ linkedlist.go
@@ -1,6 +1,10 @@
-package main
+package linkedlist
 
 import "fmt"
+
+type indexOutOfRange struct {
+	message string
+}
 
 type linkedlist struct {
 	root *node
@@ -21,11 +25,46 @@ func InitList() *linkedlist {
 	l := new(linkedlist)
 	l.root = nil
 	return l
-
 }
 
+func initError(msg string) *indexOutOfRange {
+	err := new(indexOutOfRange)
+	err.message = msg
+	return err
+}
 
-func main(){
-	fmt.Println("Testing...")
-} 
+func (l *linkedlist) Add(data interface{}) {
+	element := initNode(data)
+	if l.root == nil {
+		l.root = element
+	} else {
+		curr := l.root
+		for {
+			if(curr.next == nil) {
+				break
+			}
+			curr = curr.next
+		}
+		curr.next = element
+	}
+}
+
+func (l *linkedlist) Get(index int) (data interface{}, err *indexOutOfRange) {
+	if l.root == nil {
+		return nil, initError("List is empty!")
+	}
+	if index == 0 {
+		return l.root.data, initError("")
+	}else {
+		curr := l.root
+		for i := 0; i < index; i++ {
+			if curr.next == nil {
+				return nil, initError("Index out of Range!")
+			}
+			curr = curr.next
+		}
+		return curr.data, initError("")
+	}
+}
+
 
